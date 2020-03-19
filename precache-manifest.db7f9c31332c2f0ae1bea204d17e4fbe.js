db4ba2c5cf8729ac520679689148679c8482c242
anggaadypratama <anggaadypratama@outlook.com>
2020-03-19 14:23:43 +0700

    Updates

---
+++ precache-manifest.db7f9c31332c2f0ae1bea204d17e4fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b86317ee0351d5dc50be5e385e8a7e7",
+    "url": "/ngapalin/index.html"
+  },
+  {
+    "revision": "fa836018bdc9acc6462f",
+    "url": "/ngapalin/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "03913f0e8f64684fc43d",
+    "url": "/ngapalin/static/css/main.e2c78fce.chunk.css"
+  },
+  {
+    "revision": "fa836018bdc9acc6462f",
+    "url": "/ngapalin/static/js/2.e8924229.chunk.js"
+  },
+  {
+    "revision": "c54e23c76a8858906ad79ae3362934ce",
+    "url": "/ngapalin/static/js/2.e8924229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03913f0e8f64684fc43d",
+    "url": "/ngapalin/static/js/main.a4344497.chunk.js"
+  },
+  {
+    "revision": "1ff0e8a31625c9da2091",
+    "url": "/ngapalin/static/js/runtime-main.5bd8f282.js"
+  }
+]);
